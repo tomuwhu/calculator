f4836cd04a7c69cbbede32c3ec009c0e55f85402
Dr. Németh Tamás <tomuwhu@gmail.com>
2023-07-01 22:50:23 +0200

    .

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 D = document
 f = t => {
     s = t.innerHTML
-    if (s == `=`) D.querySelectorAll("input")[0].value = eval(D.querySelectorAll("input")[0].value)
+    ex = D.querySelectorAll("input")[0].value.replace(/√/gi,"Math.sqrt(")
+    while (Array.from(ex).filter(v => v == "(").length > (Array.from(ex).filter(v => v == ")"))) ex += ")"
+    if (s == `=`) D.querySelectorAll("input")[0].value = eval(ex)
     else if (s == `C`) D.querySelectorAll("input")[0].value = ""
     else D.querySelectorAll("input")[0].value += t.innerHTML
 }
 D.addEventListener("DOMContentLoaded", () => {
     D.querySelectorAll("h1")[0].insertAdjacentHTML("afterend", `
         <table><tr><th colspan=4><input></th></tr>
-            ${"123+|456-|789*|C0/=".split("|").map(v => 
+            ${"()√+|123-|456*|789/|C0.=".split("|").map(v => 
             `<tr>${Array.from(v).map(q => `<td onclick="f(this)">${q}</td>`).join("")}</tr>`
         ).join("")}</table>
     `)
